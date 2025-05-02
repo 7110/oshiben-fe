aa9e40f08e11bb5acddd9d980de9647de80d751f
Naito Hidehiko <hidehiko@7110.email>
2025-05-02 16:05:57 +0900

    update

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,15 +1,92 @@
 // import Image from "next/image";
-import styles from "./page.module.css";
+import styles from './page.module.css';
 
-import FloatingNav from "@/components/FloatingNav";
+import FloatingNav from '@/components/FloatingNav';
+import Messages from '@/components/Messages';
 
-import { withBasePath } from "@/utils";
+import { MessageType } from '@/domain/Message';
+
+import { withBasePath } from '@/utils';
 
 export default function Chat() {
-    return (
-        <main className={styles.ChatPage} style={ {backgroundImage: `url(${withBasePath('/background.png')})`}}>
-            
-            <FloatingNav></FloatingNav>
-        </main>
-    )
+  const messages: MessageType[] = [
+    {
+      message: 'Hello, how can I help you?',
+      type: 'ai',
+    },
+    {
+      message: 'I need help with my order.',
+      type: 'human',
+    },
+    {
+      message: 'Sure, can you provide me with your order number?',
+      type: 'ai',
+    },
+    {
+      message: 'My order number is 12345.',
+      type: 'human',
+    },
+
+    {
+      message: 'Hello, how can I help you?',
+      type: 'ai',
+    },
+    {
+      message: 'I need help with my order.',
+      type: 'human',
+    },
+    {
+      message: 'Sure, can you provide me with your order number?',
+      type: 'ai',
+    },
+    {
+      message: 'My order number is 12345.',
+      type: 'human',
+    },
+    {
+      message: 'Hello, how can I help you?',
+      type: 'ai',
+    },
+    {
+      message: 'I need help with my order.',
+      type: 'human',
+    },
+    {
+      message: 'Sure, can you provide me with your order number?',
+      type: 'ai',
+    },
+    {
+      message: 'My order number is 12345.',
+      type: 'human',
+    },
+    {
+      message: 'Hello, how can I help you?',
+      type: 'ai',
+    },
+    {
+      message: 'I need help with my order.',
+      type: 'human',
+    },
+    {
+      message: 'Sure, can you provide me with your order number?',
+      type: 'ai',
+    },
+    {
+      message: 'My order number is 12345.',
+      type: 'human',
+    },
+  ];
+
+  return (
+    <main
+      className={styles.chat}
+      style={{ backgroundImage: `url(${withBasePath('/background.png')})` }}
+    >
+      <div className={styles.chat__bottom}>
+        <FloatingNav></FloatingNav>
+
+        <Messages className={styles.chat__messages} messages={messages} />
+      </div>
+    </main>
+  );
 }
