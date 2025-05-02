79a70b00553f15eb22bfd4e51a1c8b116f1e70e9
Naito Hidehiko <hidehiko@7110.email>
2025-05-02 15:31:20 +0900

    fix

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,12 +1,15 @@
-import Image from "next/image";
+// import Image from "next/image";
 import styles from "./page.module.css";
+
+import FloatingNav from "@/components/FloatingNav";
 
 import { withBasePath } from "@/utils";
 
 export default function Chat() {
     return (
         <main className={styles.ChatPage} style={ {backgroundImage: `url(${withBasePath('/background.png')})`}}>
-            aa
+            
+            <FloatingNav></FloatingNav>
         </main>
     )
 }
---
+++ src/components/FloatingNav/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function FloatingNav() { 
+    return (
+        <div className="floating-nav">
+        <div className="floating-nav__item">
+            <a href="#section1">Section 1</a>
+        </div>
+        <div className="floating-nav__item">
+            <a href="#section2">Section 2</a>
+        </div>
+        <div className="floating-nav__item">
+            <a href="#section3">Section 3</a>
+        </div>
+        </div>
+    );
+}
