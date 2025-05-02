5e90f5bc28dab441377fb7b3d9134d0370b3f45c
Naito Hidehiko <hidehiko@7110.email>
2025-05-02 14:47:26 +0900

    update to deploy ghpages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/oshiben-fe',
+  assetPrefix: '/oshiben-fe/',
 };
 
 export default nextConfig;
